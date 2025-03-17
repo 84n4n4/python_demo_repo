b9935c6b7a78737fe001efa357b51479a3bab2cb
Thomas Hirsch <84n4n4@gmail.com>
2025-03-17 18:51:06 +0100

    pydantic settings

---
+++ src/pydantic_settings_demo/demo_config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Config(BaseSettings):
+    DB_HOST: str = 'http://localhost:8529'
+    DB_USERNAME: str = 'root'
+    DB_PASSWORD: str = 'root_passwd'
+    DB_DATABASE: str = 'tuw'
+    # class Config:
+    #     env_file = ".env"
+    model_config = SettingsConfigDict(env_file='.env')
+
+config = Config()
---
+++ src/pydantic_settings_demo/settings_demo.py
@@ -0,0 +1,7 @@
+from demo_config import config
+
+def main():
+    print(config.DB_HOST)
+
+if __name__ == "__main__":
+    main()
